421431f35419b198f3a370748cf739f863022e16
HCLonely <h1606051253@gmail.com>
2026-07-26 15:32:53 +0800

    更新版本号

--- dist/js/artitalk.js
+++ dist/js/artitalk.js
@@ -1175,7 +1175,7 @@
   // # sourceMappingURL=showdown.min.js.map
   ;
   'use strict';
-  const atVersion = '4.0.2';
+  const atVersion = '4.1.0';
   // if (typeof (appID) !== 'undefined') {
   //   const update = confirm('　　3.x版本更新需要修改一下配置，深感抱歉，但是为了更好的体验不得不这样。\n　　如果不想更换，可以继续使用2.4.2版本，这也是比较成熟的版本了，但是以后不会再维护。更换详情请点击确定前往文档查看');
   //   if (update == true) {
